486da62040fec0906dd5680db5441f916532ca85
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2017-02-27 15:46:29 +0100

    adder projektet

---
+++ Multiplayer/Assets/Resources/Scripts/myNetworkManeger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class myNetworkManeger : NetworkManager
+{
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player;
+        PlayerOptions Options = gameObject.GetComponent<PlayerOptions>();
+        if (Options.usingVR )
+            player = Instantiate(Resources.Load("Players/VRPlayer"), transform.position, Quaternion.identity) as GameObject;
+
+        else
+            player = Instantiate(Resources.Load("Players/testPlayer"), transform.position, Quaternion.identity) as GameObject;
+
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
